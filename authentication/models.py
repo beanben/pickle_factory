74ddaf13ef4bed676ec54a5281e0019c09a261c0
Ben <base_kh@hotmail.com>
2022-11-27 12:16:53 +0000

    no message

--- authentication/models.py
+++ authentication/models.py
@@ -41,8 +41,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     @property
     def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
         return self.is_superuser
 
     
