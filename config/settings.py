22df7aaba16f69876cc812715075662fa5a93e8b
Ben <base_kh@hotmail.com>
2022-12-11 10:40:49 +0000

    updated allowed host setting

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ALLOWED_HOSTS = [
 #     '.pickle-factory.net'
 # ]
-ALLOWED_HOSTS =['*'] 
+ALLOWED_HOSTS =['www.pickle-factory.net', 'pickle-factory.net', '.pickle-factory.net'] 
 
 
 # Application definition
