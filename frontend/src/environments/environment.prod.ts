c54593954c7470e5f08f2df8f6365cccc57b806a
Ben <base_kh@hotmail.com>
2022-12-11 18:11:06 +0000

    environment.prod API URL hardcoded as 'http://pickle-factory.net/api'

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true,
   // API_BASE_URL: '/api',
    // test
-   API_BASE_URL: location.origin + '/api'
+  //  API_BASE_URL: location.origin + '/api'
+
+  API_BASE_URL: 'http://pickle-factory.net/api'
 };
 
