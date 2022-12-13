1905608353091e99cb95fcdeff5f9c6392e68bf4
Ben <base_kh@hotmail.com>
2022-12-13 06:13:49 +0000

    no message

--- config/middleware.py
+++ config/middleware.py
@@ -9,11 +9,11 @@ class DomainRedirectMiddleware:
         print('host:', host)
         print('request.path:', request.path)
         if host == "pickle-factory.herokuapp.com":
-            response = HttpResponsePermanentRedirect("pickle-factory.net" + request.path)
+            return HttpResponsePermanentRedirect("http://pickle-factory.net" + request.path)
         else:
-            response = self.get_response(request)
+            return self.get_response(request)
 
-        return response
+
 
 # Source 1: https://docs.djangoproject.com/en/4.1/topics/http/middleware/
 # Source 2: https://adamj.eu/tech/2020/03/02/how-to-make-django-redirect-www-to-your-bare-domain/#adding-a-middleware
