bd9c71a7e057e115b74779296a53aba2195ab338
Ben <base_kh@hotmail.com>
2022-12-25 11:49:54 +0100

    can create loan and close modal from home page

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/_services/auth/auth.service';
 import { User } from '../auth/user';
+import { Loan } from '../loan/loan';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +10,17 @@ import { User } from '../auth/user';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  openPopup = false;
+  openLoanModal = false;
   user = {} as User;
   isLoggedIn = false;
 
   constructor(
-    public _authService: AuthService
-  ) { }
+    public _authService: AuthService,
+    private el: ElementRef,
+    private router: Router
+  ) { 
+    this.addEventBackgroundClose()
+  }
 
   ngOnInit(): void {
     this.isLoggedIn = this._authService.isLoggedIn();
@@ -36,7 +42,23 @@ export class HomeComponent implements OnInit {
   };
 
   closePopup(){
-    this.openPopup = false;
+    this.openLoanModal = false;
+  }
+
+  addEventBackgroundClose(){
+    this.el.nativeElement.addEventListener('click', (el:any) => {
+      if (el.target.className === 'modal') {
+          this.closePopup();
+      }
+    });
+  };
+
+  onSave(loan:Loan | null){
+    this.openLoanModal = false;
+
+    if(!!loan){
+      this.router.navigate(['/loan'])
+    }
   }
 
 }
